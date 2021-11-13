615f60572cc13a5e7b805c920feef0a0a8ee86e5
christian <christianmereles@gmail.com>
2021-11-12 21:22:43 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Chris, how are you?
         </p>
+        <h1>holaaaaa</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
