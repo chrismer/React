e679993459b3fa5b7fdbc4942580542c188d5fc5
christian <christianmereles@gmail.com>
2021-11-22 22:05:27 -0300

    modificando itemListContainer

--- src/components/Hero/ItemListContainer.js
+++ src/components/Hero/ItemListContainer.js
@@ -1,9 +1,13 @@
+import { faSmileWink } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import './ItemListContainer.css'
 
 const ItemListContainer = ({greeting}) => {
     return (
         <div>
-            <h1>{greeting}</h1>
+            <h1 className="greeting">{greeting}</h1>
+            <FontAwesomeIcon className="smile" icon={faSmileWink}/>
         </div>
     )
 }
