346089119bef8011612fd42a87a7186593030451
christian <christianmereles@gmail.com>
2021-12-15 23:16:43 -0300

    desafio 9

--- src/components/ItemDetails/ItemDetail.js
+++ src/components/ItemDetails/ItemDetail.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 import ItemCount from '../Counter/ItemCount'
 import './ItemDetail.css'
 
+// const cartButton = () => {
+//     const [counter, setCounter] = useState(0);
+    
+//     const handleChange = (e) => {
+        
+//     }
+//     return (
+//         <div className='contador'>
+//             <ItemCount stock={10} initial={0} onAdd={handleChange}/>
+//         </div>
+//     )
+// }
+
+
 const ItemDetail = ({product}) => {
+    // const [counter, setCounter] = useState(true);
+    const [showButton, setShowButton] = useState(true);
+
+    const onAdd = () => {
+        setShowButton(false);
+    }
+
+    const addToCart = (count)=>{
+        setShowButton(false);  
+    }
+
     return (
         <div className='ItemCard'>
             <h1>Item Detail</h1>
@@ -10,7 +36,9 @@ const ItemDetail = ({product}) => {
             <h2 className='ItemCard__title'>{product?.name}</h2> 
             <p className='ItemCard__price'>{product?.price}</p>
             <p className='descripcion'>{product?.descripcion}</p>
-            <ItemCount/>
+            {/* <ItemCount stock={10} initial={0} onAdd={addToCart}/> */}
+            {showButton ? <ItemCount onAdd={onAdd}/> : <Link to='/cart'>Finalizar Compra</Link>}
+    
         </div>
     )
 }
